cf4f66c92df92f86d333ed3ae442134d51101c61
Ayush Kumar <162584996+ayush-244@users.noreply.github.com>
2026-07-08 01:52:38 +0530

    Readme

--- client/src/components/ContactSection.jsx
+++ client/src/components/ContactSection.jsx
@@ -72,7 +72,7 @@ export const ContactSection = () => {
     setIsSubmitting(true);
 
     try {
-      const response = await fetch('https://formspree.io/f/xwpbojaj', {
+      const response = await fetch('https://formspree.io/f/mgojbavz', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
